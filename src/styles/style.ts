eaff826594ca24cbea766b1e3c4ed53cda67f5b9
daniel <danieldubonxd@gmail.com>
2023-06-12 13:38:39 -0600

    yarn lint

--- src/styles/style.ts
+++ src/styles/style.ts
@@ -1,7 +1,6 @@
 const styles = {
 	boxWidth:
 		'flex flex-col relative h-screen text-center md:text-left md:flex-row  px-10 justify-evenly mx-auto items-center overflow-hidden',
-	titleSection:
-		'text-2xl font-medium md:text-4xl md:leading-tight'
+	titleSection: 'text-2xl font-medium md:text-4xl md:leading-tight'
 }
 export default styles
